a7f376166a1d1a78f8c0cf5937f45bf4aa3651ec
Christoph Lukas <lukas@pingworks.de>
2016-03-26 09:55:31 +0100

    bump version to 0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@pingworks.de'
 license 'Apache License, Version 2.0'
 description 'Installs/Configures chef-dash'
 long_description 'Installs/Configures chef-dash'
-version '0.3.10'
+version '0.5.0'
 
 supports 'debian'
 
